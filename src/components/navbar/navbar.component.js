eea56016469b4175cec40e6e920854ed4a96f98d
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-01-15 12:18:28 +0100

    Initial commit

---
+++ src/components/navbar/navbar.component.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <img
+      src="../../../Ayoub.ico"
+      style={{
+        width: "180px",
+        height: "180px",
+        color: "black",
+        borderRadius: "50%",
+        margin: "5px",
+      }}
+      alt="logo"
+    />
+  );
+}
