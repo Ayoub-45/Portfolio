7347570ac6dfbd1945f1890b8f3ce544e4947e64
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-11 10:07:34 +0100

    complete the portfolio

--- src/pages/contact/contact.component.js
+++ src/pages/contact/contact.component.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import "./contact.styles.css"; // Import your CSS file where the styles will be defined
+import { sendEmail } from "../../helpers/sendEmail";
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    to_name: "",
+    user_email: "",
+    message: "",
+  });
 
-function Contact() {
-  return <div>Contact</div>;
-}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendEmail(e);
+    // Reset form after submission if needed
+    setFormData({
+      to_name: "",
+      user_email: "",
+      message: "",
+    });
+  };
+
+  return (
+    <form className="custom-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="to_name"
+        placeholder="Recipient Name"
+        value={formData.to_name}
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        name="user_email"
+        placeholder="Your Email"
+        value={formData.user_email}
+        onChange={handleChange}
+      />
+      <textarea
+        name="message"
+        placeholder="Your Message"
+        value={formData.message}
+        onChange={handleChange}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+};
 export default Contact;
